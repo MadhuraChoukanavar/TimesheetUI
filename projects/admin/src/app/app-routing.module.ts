360d35abf634a892596c52d1621cc81858d8cd6b
Madhura Choukanavar <Choukanavarmadhura6@gmail.com>
2024-02-10 14:05:51 +0530

    m

--- projects/admin/src/app/app-routing.module.ts
+++ projects/admin/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { EmployeeDisplayComponent } from './employee-display/employee-display.co
 import { AddEmployeeComponent } from './add-employee/add-employee.component';
 import { AccountDisplayComponent } from './account-display/account-display.component';
 import { AddAccountComponent } from './add-account/add-account.component';
+import { TimesheetComponent } from './timesheet/timesheet.component';
 
 
 
@@ -15,10 +16,10 @@ const routes: Routes = [
   { path: 'login', component: LoginPageComponent },
   {path:'dashboard',component:DashBoardComponent},
   {path:'employeedisplay',component:EmployeeDisplayComponent},
- 
  {path:'addemployee',component:AddEmployeeComponent},
   {path:'accountdisplay',component:AccountDisplayComponent},
   {path:'addaccount',component:AddAccountComponent},
+  {path:'timesheet',component:TimesheetComponent},
   { path:'', component: HomePageComponent },
   { path:'**', component: HomePageComponent },
 ];
